0b77a6748f357d40b8158af9c94e04571e1647f8
Matthias Nunes <matthiasnunes@gmail.com>
2015-01-27 17:55:15 -0200

    Changes name of delete to remove

--- include/GTree.h
+++ include/GTree.h
@@ -7,7 +7,7 @@ typedef struct GTree{
 
 GTree* insert(GTree* root, int data);
 GTree* contains(GTree* root, int data);
-GTree* delete(GTree* root, int data);
+GTree* remove(GTree* root, int data);
 void print(GTree* root);
 GTree* skew(GTree* root);
 GTree* split(GTree* root);
