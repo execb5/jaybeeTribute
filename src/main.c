b18e033934c425df250e94d0dc754648c694819b
Matthias Nunes <matthiasnunes@gmail.com>
2015-02-03 18:26:14 -0200

    New test in main, using numerical order

--- src/main.c
+++ src/main.c
@@ -4,19 +4,37 @@ int main(int argc, const char *argv[])
 {
         GTree* g;
         g = initialize();
-        g = insert(g, 2);
-        g = insert(g, 5);
-        g = insert(g, 4);
-        g = insert(g, 7);
-        g = insert(g, 6);
-        g = insert(g, 3);
-        g = insert(g, 8);
-        g = insert(g, 10);
-        g = insert(g, 9);
         g = insert(g, 1);
         cutePrint(g, "");
-        g = remove(g, 10);
         printf("\n-------------------\n\n");
+        g = insert(g, 2);
         cutePrint(g, "");
+        printf("\n-------------------\n\n");
+        g = insert(g, 3);
+        cutePrint(g, "");
+        printf("\n-------------------\n\n");
+        g = insert(g, 4);
+        cutePrint(g, "");
+        printf("\n-------------------\n\n");
+        g = insert(g, 5);
+        cutePrint(g, "");
+        printf("\n-------------------\n\n");
+        g = insert(g, 6);
+        cutePrint(g, "");
+        printf("\n-------------------\n\n");
+        g = insert(g, 7);
+        cutePrint(g, "");
+        printf("\n-------------------\n\n");
+        g = insert(g, 8);
+        cutePrint(g, "");
+        printf("\n-------------------\n\n");
+        g = insert(g, 9);
+        cutePrint(g, "");
+        printf("\n-------------------\n\n");
+        g = insert(g, 10);
+        cutePrint(g, "");
+        printf("\n-------------------\n\n");
+        /*g = remove(g, 10);*/
+        /*cutePrint(g, "");*/
         return 0;
 }
